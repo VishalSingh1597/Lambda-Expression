f4e9455a62a9a7aaa99e828d9074f212d482e691
VishalSingh1597 <80283079+VishalSingh1597@users.noreply.github.com>
2021-05-03 09:11:29 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,45 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lambda_Expressions
+namespace LambdaExpressionProblem
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("-----Welcome To Lambda Expression-----");
+            Console.WriteLine();
+
+            //list object creation with person 
+            List<Person> list = new List<Person>();
+
+            //method call
+            AddingPersonDetails(list);
+
+            Console.ReadLine();
         }
+
+        //UC1 person management
+        public static void AddingPersonDetails(List<Person> personList) //creating method
+        {
+            personList.Add(new Person() { SSN = 1, Age = 23, Name = "Vishal", Address = "Mumbai" });
+            personList.Add(new Person() { SSN = 2, Age = 24, Name = "Himanshu", Address = "Nainital" });
+            personList.Add(new Person() { SSN = 3, Age = 19, Name = "Omprakash", Address = "Pune" });
+            personList.Add(new Person() { SSN = 5, Age = 75, Name = "Ekta", Address = "Pune" });
+            personList.Add(new Person() { SSN = 6, Age = 86, Name = "Abhishek", Address = "Nashik" });
+            personList.Add(new Person() { SSN = 7, Age = 15, Name = "roshan", Address = "UttarPradesh" });
+            IterateOverList(personList);
+        }
+        public static void IterateOverList(List<Person> personlist)
+        {
+            foreach (Person person in personlist)
+            {
+                Console.WriteLine("Age\t" + person.Age + "\t" + "Name\t" + person.Name + "\t" + "Address\t" + person.Address);
+            }
+        }
+
     }
 }
