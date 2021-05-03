2c037282225455ea31fa1b30dc840224e84ada63
VishalSingh1597 <80283079+VishalSingh1597@users.noreply.github.com>
2021-05-03 09:09:36 +0530

    Update Person.cs

--- Person.cs
+++ Person.cs
@@ -1,12 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lambda_Expressions
+namespace LambdaExpressionProblem
 {
-    class Person
+  public   class Person
     {
+
+        public int SSN { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
     }
 }
